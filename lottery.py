2d5af8240600eb7ff3510131181b475b29fcab45
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-20 02:04:58 -0500

    Add functions // Clean code

--- lottery.py
+++ lottery.py
@@ -9,17 +9,29 @@ robbie_soup = BeautifulSoup(robbie_page.content, 'html.parser')
 lands_lot_soup = BeautifulSoup(lands_lot_page.content, 'html.parser')
 
 print("Robbie's Lottery Winners :: " + robbie_soup.find('div', class_='title wegadnumber').text)
+
 robbie_drawing = robbie_soup.find('div', class_='drawings four').text
+
 drawingList = []
-for x in robbie_drawing:
-        drawingList.append(x)
+
+def populateDrawingList(list):
+        for x in robbie_drawing:
+                list.append(x)
+        return
+
+populateDrawingList(drawingList)
 
 # remove unncecessary elements
-for x in drawingList:
-         drawingList.remove("\n") 
-drawingList.pop(11)  
-drawingList.pop(len(drawingList)-2)
-drawingList.pop(len(drawingList)-1)
+def removeElements(list):
+        for x in list:
+                list.remove("\n") 
+        
+        list.pop(11)  
+        list.pop(len(drawingList)-2)
+        list.pop(len(drawingList)-1)
+        return
+
+removeElements(drawingList)
 
 print("\n1st Prize: {}".format(drawingList[0]+drawingList[1]+drawingList[2]+drawingList[3]))
 print("\n2nd Prize: {}".format(drawingList[4]+drawingList[5]+drawingList[6]+drawingList[7]))
@@ -31,40 +43,35 @@ images1 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning1'
 images2 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning2')
 images3 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning3')
 
+def addToList(list):
+        for img in images1.find_all('img'):
+                list.append(img['src'])
+        return
+
 Winners1 = []
-for img in images1.find_all('img'):
-        Winners1.append(img['src'])
-
 Winners2 = []
-for img in images2.find_all('img'):
-        Winners2.append(img['src'])
-
 Winners3 = []
-for img in images3.find_all('img'):
-        Winners3.append(img['src'])
+
+addToList(Winners1)
+addToList(Winners2)
+addToList(Winners3)
 
 # bad characters
 bad_chars = ['../images/black/','.jpg']
 
 # remove bad characters
-for i in bad_chars: 
-        Winners1[0] = Winners1[0].replace(i,'')
-        Winners1[1] = Winners1[1].replace(i,'')
-        Winners1[2] = Winners1[2].replace(i,'')
-        Winners1[3] = Winners1[3].replace(i,'')
-        Winners1[4] = Winners1[4].replace(i,'')
+def removeBadChars(list):
+        for i in bad_chars: 
+                list[0] = list[0].replace(i,'')
+                list[1] = list[1].replace(i,'')
+                list[2] = list[2].replace(i,'')
+                list[3] = list[3].replace(i,'')
+                list[4] = list[4].replace(i,'')
+        return
 
-        Winners2[0] = Winners2[0].replace(i,'')
-        Winners2[1] = Winners2[1].replace(i,'')
-        Winners2[2] = Winners2[2].replace(i,'')
-        Winners2[3] = Winners2[3].replace(i,'')
-        Winners2[4] = Winners2[4].replace(i,'')
-
-        Winners3[0] = Winners3[0].replace(i,'')
-        Winners3[1] = Winners3[1].replace(i,'')
-        Winners3[2] = Winners3[2].replace(i,'')
-        Winners3[3] = Winners3[3].replace(i,'')
-        Winners3[4] = Winners3[4].replace(i,'')
+removeBadChars(Winners1)
+removeBadChars(Winners2)
+removeBadChars(Winners3)
 
 print("\n1st Prize: {}".format(Winners1[0]+Winners1[1]+Winners1[2]+Winners1[3]+Winners1[4]))
 print("\n2nd Prize: {}".format(Winners2[0]+Winners2[1]+Winners2[2]+Winners2[3]+Winners2[4]))
