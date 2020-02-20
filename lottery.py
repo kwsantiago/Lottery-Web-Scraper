d97b625637c25eb24f8bc41d4f158644aae01493
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-20 11:39:05 -0500

    Added functions // Cleaned code

--- lottery.py
+++ lottery.py
@@ -12,7 +12,14 @@ print("Robbie's Lottery Winners :: " + robbie_soup.find('div', class_='title weg
 
 robbie_drawing = robbie_soup.find('div', class_='drawings four').text
 
+images1 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning1')
+images2 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning2')
+images3 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning3')
+
 robbies_winners = []
+lands_lot_winners1 = []
+lands_lot_winners2 = []
+lands_lot_winners3 = []
 
 def populate_robbies_winners(list):
         for x in robbie_drawing:
@@ -57,41 +64,36 @@ def removeBadChars(list):
                 list[4] = list[4].replace(i,'')
         return
 
+def print_robbies_winners(winners):
+        print("\n1st Prize: {}".format(winners[0]+winners[1]+winners[2]+winners[3]))
+        print("\n2nd Prize: {}".format(winners[4]+winners[5]+winners[6]+winners[7]))
+        print("\n3rd Prize: {}".format(winners[8]+winners[9]+winners[10]+winners[11]))
+        return
+
+def print_lands_lot_winners(winners):
+        print("\n1st Prize: {}".format(winners[0]+winners[1]+winners[2]+winners[3]+winners[4]))
+        return  
+
 populate_robbies_winners(robbies_winners)
 removeElements(robbies_winners)
-
-print("\n1st Prize: {}".format(robbies_winners[0]+robbies_winners[1]+robbies_winners[2]+robbies_winners[3]))
-print("\n2nd Prize: {}".format(robbies_winners[4]+robbies_winners[5]+robbies_winners[6]+robbies_winners[7]))
-print("\n3rd Prize: {}".format(robbies_winners[8]+robbies_winners[9]+robbies_winners[10]+robbies_winners[11]))
-
-print("---------------------------")
-print("Landsloterij Winners")
-images1 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning1')
-images2 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning2')
-images3 = lands_lot_soup.find('span', id='ctl00_ContentPlaceHolder1_lblWinning3')
-
-Winners1 = []
-Winners2 = []
-Winners3 = []
-
-populate_lands_winners1(Winners1)
-populate_lands_winners2(Winners2)
-populate_lands_winners3(Winners3)
-
-removeBadChars(Winners1)
-removeBadChars(Winners2)
-removeBadChars(Winners3)
-
-print("\n1st Prize: {}".format(Winners1[0]+Winners1[1]+Winners1[2]+Winners1[3]+Winners1[4]))
-print("\n2nd Prize: {}".format(Winners2[0]+Winners2[1]+Winners2[2]+Winners2[3]+Winners2[4]))
-print("\n2nd Prize: {}".format(Winners3[0]+Winners3[1]+Winners3[2]+Winners3[3]+Winners3[4]))
+print_robbies_winners(robbies_winners)
+print("---------------------------\nLandsloterij Winners")
+populate_lands_winners1(lands_lot_winners1)
+populate_lands_winners2(lands_lot_winners2)
+populate_lands_winners3(lands_lot_winners3)
+removeBadChars(lands_lot_winners1)
+removeBadChars(lands_lot_winners2)
+removeBadChars(lands_lot_winners3)      
+print_lands_lot_winners(lands_lot_winners1)
+print_lands_lot_winners(lands_lot_winners2)
+print_lands_lot_winners(lands_lot_winners3)
 
 # send to .csv
 workbook = xlsxwriter.Workbook('output.csv')
 worksheet = workbook.add_worksheet()
 row = 0
 array = [["Robbie's Winners",(robbies_winners[0]+robbies_winners[1]+robbies_winners[2]+robbies_winners[3]),(robbies_winners[4]+robbies_winners[5]+robbies_winners[6]+robbies_winners[7]),(robbies_winners[8]+robbies_winners[9]+robbies_winners[10]+robbies_winners[11])],
-         ['Landsloterij Winners',(Winners1[0]+Winners1[1]+Winners1[2]+Winners1[3]+Winners1[4]),(Winners2[0]+Winners2[1]+Winners2[2]+Winners2[3]+Winners2[4]),(Winners3[0]+Winners3[1]+Winners3[2]+Winners3[3]+Winners3[4])]]
+         ['Landsloterij Winners',(lands_lot_winners1[0]+lands_lot_winners1[1]+lands_lot_winners1[2]+lands_lot_winners1[3]+lands_lot_winners1[4]),(lands_lot_winners2[0]+lands_lot_winners2[1]+lands_lot_winners2[2]+lands_lot_winners2[3]+lands_lot_winners2[4]),(lands_lot_winners3[0]+lands_lot_winners3[1]+lands_lot_winners3[2]+lands_lot_winners3[3]+lands_lot_winners3[4])]]
 
 for col, data in enumerate(array):
     worksheet.write_column(row, col, data)
