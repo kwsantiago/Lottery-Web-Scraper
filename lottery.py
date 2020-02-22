5312e72f100d9f2a8cff17c3fd35a2d18e57b14e
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-02-21 20:47:40 -0500

    Center table in HTML/CSS

--- lottery.py
+++ lottery.py
@@ -73,7 +73,7 @@ def print_lands_lot_winners(winners1,winners2,winners3):
         return  
 
 def array2htmltable(data):
-    q = '<table align="center" border="1">\n'
+    q = '<style>h1 {left: 0;line-height: 200px;margin-top: -100px;position: absolute;text-align: center;top: 50%;width: 100%; }</style><h1><table align="center" border="1">\n'
     for i in [(data[0:1], 'td'), (data[1:], 'td')]:
         q += "\n".join(
             [
@@ -87,7 +87,7 @@ def array2htmltable(data):
                     ) for _m in i[0]
                 ] 
             ])+"\n"
-    q += "</table>"
+    q += "</table></h1>"
     return q
 
 def to_html(array):
