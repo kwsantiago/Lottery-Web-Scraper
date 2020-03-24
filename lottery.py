b70fc4f09d69441157d3b8fe59e996d8665647b3
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-24 16:54:37 -0400

    Add comments

--- lottery.py
+++ lottery.py
@@ -63,7 +63,7 @@ def print_all_lands_winners():
         print_lands_winners3()
         return
 
-def array2htmltable(data):
+def array2htmltable(data): 
     q = '<style>h1 {left: 0;line-height: 200px;margin-top: -100px;position: absolute;text-align: center;top: 50%;width: 100%; }</style><h1><table align="center" border="1">\n'
     for i in [(data[0:], 'td')]:
         q += "\n".join(
@@ -83,19 +83,19 @@ def array2htmltable(data):
 
 def to_html():
         robbie_drawing = getRobbieData()
-        robbies_winners = re.sub('[\n]','', robbie_drawing)
+        robbies_winners = re.sub('[\n]','', robbie_drawing) # remove unnecessary characeters
         images1 = getLandsWinners1()
-        lands_winners1 = re.sub('[a-zA-Z..<>/"=_ ]','', images1)
+        lands_winners1 = re.sub('[a-zA-Z..<>/"=_ ]','', images1) # remove unnecessary characeters
         images2 = getLandsWinners2()
-        lands_winners2 = re.sub('[a-zA-Z..<>/"=_ ]','', images2)
+        lands_winners2 = re.sub('[a-zA-Z..<>/"=_ ]','', images2) # remove unnecessary characeters
         images3 = getLandsWinners3()
-        lands_winners3 = re.sub('[a-zA-Z..<>/"=_ ]','', images3)
+        lands_winners3 = re.sub('[a-zA-Z..<>/"=_ ]','', images3) # remove unnecessary characeters
 
         array = [["Robbie's Winners",("1: "+ robbies_winners[0:4]),("2: " + robbies_winners[5:9]),("3: " + robbies_winners[8:12])],
          ['Landsloterij Winners',("1: " + lands_winners1[4:9]),("2: " + lands_winners2[4:9]),("3: " + lands_winners3[4:9])]]
         html_str = array2htmltable(array)
         print(html_str)
-        Html_file= open("/var/www/html/output.html","w")
+        Html_file= open("/var/www/html/output.html","w") # send the output to the the html folder for PHP server
         Html_file.write(html_str)
         Html_file.close()
         return
